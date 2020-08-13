1bcf42367391216812dc96dcc407d4a28ffd1fcb
vovan55 <mr.vovan55@gmail.com>
2020-08-13 23:42:58 +0600

    Added onClickListener

--- app/src/main/java/com/pokerhelper/thebest/MainActivity.java
+++ app/src/main/java/com/pokerhelper/thebest/MainActivity.java
@@ -4,16 +4,45 @@ import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements NavItemSelectedListener{
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
         setupMenu();
     }
+    private void init()
+    {
+        ImageButton iBtnHearts = findViewById(R.id.iBtnHearts);
+        ImageButton iBtnDiamonds = findViewById(R.id.iBtnDiamonds);
+        ImageButton iBtnClubs = findViewById(R.id.iBtnClubs);
+        ImageButton iBtnSpades = findViewById(R.id.iBtnSpades);
+    }
+    public void onClickClubs(View view)
+    {
+        
+    }
+    public void onClickHearts(View view)
+    {
+
+    }
+    public void onClickDiamonds(View view)
+    {
+
+    }
+    public void onClickSpades(View view)
+    {
+
+    }
+
+
     private void setupMenu() {
         FragmentManager fm = getSupportFragmentManager();
         MenuFragmentList mMenuFragment = (MenuFragmentList) fm.findFragmentById(R.id.id_container_menu);
